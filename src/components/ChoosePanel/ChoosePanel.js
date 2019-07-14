45e32ee84292651cf94438a451f3b583cfdfa2ce
Yvonne Yu <monkey6277@gmail.com>
2019-07-14 20:37:44 +0800

    update selector and input styles

--- src/components/ChoosePanel/ChoosePanel.js
+++ src/components/ChoosePanel/ChoosePanel.js
@@ -4,19 +4,21 @@ import styles from './ChoosePanel.scss';
 const choosePanel  = (props) => {
   return (
     <form>
-      <lable for="res_name">餐廳名稱
-        <input type="text" name="res_name" id="res_name" placeholder="請輸入餐廳名稱..."/>
-      </lable>
-      <lable>價格
-        <div className="button-group panel-select">
+      <div className="grid-x grid-padding-x">
+        <lable htmlFor="res_name">餐廳名稱
+          <input type="text" name="res_name" id="res_name" placeholder="請輸入餐廳名稱..."/>
+        </lable>
+      </div>
+      <div className="grid-x grid-padding-x panel-select">
+        <fieldset className="large-5 cell">價格
           <input id="checkbox1" type="checkbox"/>
-          <label className="button" for="checkbox1">便宜</label>
+          <label className="button" htmlFor="checkbox1">便宜</label>
           <input id="checkbox2" type="checkbox"/>
-          <label className="button" for="checkbox2">還可以</label>
+          <label className="button" htmlFor="checkbox2">還可以</label>
           <input id="checkbox3" type="checkbox"/>
-          <label className="button" for="checkbox3">貴</label>
-        </div>
-      </lable>
+          <label className="button" htmlFor="checkbox3">貴</label>
+        </fieldset>
+      </div>
     </form>
   )
 };
