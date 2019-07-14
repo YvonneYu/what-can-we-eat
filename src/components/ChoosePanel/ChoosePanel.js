e82950da831822edf6fe3c5f86615d46e86e406c
Yvonne Yu <monkey6277@gmail.com>
2019-07-14 21:29:46 +0800

    update handleSelect with setState to recode selector change

--- src/components/ChoosePanel/ChoosePanel.js
+++ src/components/ChoosePanel/ChoosePanel.js
@@ -3,8 +3,19 @@ import styles from './ChoosePanel.scss';
 
 const choosePanel  = (props) => {
 
+  let selectorInputCom = props.priceValues.map( (value, index) => {
+    let id = 'checkbox'+ index;
+    return (
+      <React.Fragment>
+        <input key={index} id={ id } type="checkbox"
+               checked={ value.checked } onChange={ () => props.onSelect(index) } />
+        <label className="button" htmlFor={ id } >{ value.label }</label>
+      </React.Fragment>
+    )
+  });
+
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={ props.onSubmit }>
       <div className="grid-x grid-padding-x">
         <lable htmlFor="res_name">餐廳名稱
           <input type="text" name="res_name" id="res_name" placeholder="請輸入餐廳名稱..."/>
@@ -12,12 +23,7 @@ const choosePanel  = (props) => {
       </div>
       <div className="grid-x grid-padding-x panel-select">
         <fieldset className="large-5 cell">價格
-          <input id="checkbox1" type="checkbox"/>
-          <label className="button" htmlFor="checkbox1">便宜</label>
-          <input id="checkbox2" type="checkbox"/>
-          <label className="button" htmlFor="checkbox2">還可以</label>
-          <input id="checkbox3" type="checkbox"/>
-          <label className="button" htmlFor="checkbox3">貴</label>
+          { selectorInputCom }
         </fieldset>
       </div>
       <div>
--- src/containers/ChoosePanelBuilder/ChoosePanelBuilder.js
+++ src/containers/ChoosePanelBuilder/ChoosePanelBuilder.js
@@ -3,13 +3,41 @@ import ChoosePanel from '../../components/ChoosePanel/ChoosePanel';
 
 class choosePanelBuilder extends Component {
 
+  state = {
+    priceValues: [
+      {
+        label: '便宜',
+        checked: false
+      },
+      {
+        label: '還可以',
+        checked: false
+      },
+      {
+        label: '貴',
+        checked: false
+      }
+    ]
+  };
+
+  onSelectorChange = (targetIndex) => {
+    let newValues = this.state.priceValues.map((value, i) => {
+        return targetIndex !== i ? value: {...value, checked: !value.checked};
+    });
+    this.setState( { priceValues: newValues });
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
   };
 
   render () {
-    return <ChoosePanel onSubmit={ this.handleSubmit }></ChoosePanel>;
+    return <ChoosePanel
+      {...this.state}
+      onSubmit={ this.handleSubmit}
+      onSelect={ this.onSelectorChange }
+    ></ChoosePanel>;
   }
 
 }
