71a2479a59e40981faeaac2408db1fc20734254b
Yvonne Yu <monkey6277@gmail.com>
2019-07-15 21:19:48 +0800

    add topbar styles and html

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Layout.scss';
 
 const layout = ( props ) => {
   return (
     <React.Fragment>
-      <div>Toolbar, 漢堡bar 等上方欄</div>
-      <main>
+      <div className="top-bar my-top-bar">
+        <div className="top-bar-left">
+          <h3 className="top-bar-title">隨便想吃什麼？</h3>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li><a href="#">吃什麼？</a></li>
+            <li><a href="#">編輯餐廳</a></li>
+          </ul>
+        </div>
+      </div>
+      <main className="my-main">
         { props.children }
       </main>
     </React.Fragment>
