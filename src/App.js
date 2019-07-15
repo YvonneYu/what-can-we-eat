6c04d0edd6d4ab26968c6a2a6361a04f5b16640e
Yvonne Yu <monkey6277@gmail.com>
2019-07-15 11:58:44 +0800

    import foundation using webpack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss';
 import Layout from './components/Layout/Layout.js';
 import RestaurantsBuilder from './containers/RestaurantsBuilder/RestaurantsBuilder';
 import ChoosePanelBuilder from './containers/ChoosePanelBuilder/ChoosePanelBuilder';
