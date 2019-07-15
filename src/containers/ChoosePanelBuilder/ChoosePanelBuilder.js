d137f5e200fc1daab68599f6257b206393617945
Yvonne Yu <monkey6277@gmail.com>
2019-07-15 15:08:31 +0800

    restaurants choices trasform to json format

--- src/containers/ChoosePanelBuilder/ChoosePanelBuilder.js
+++ src/containers/ChoosePanelBuilder/ChoosePanelBuilder.js
@@ -1,72 +1,12 @@
 import React, { Component } from 'react';
 import ChoosePanel from '../../components/ChoosePanel/ChoosePanel';
 import './ChoosePanelBuilder.scss';
+import restChoices from '../../util/restChoices.json';
 
 class choosePanelBuilder extends Component {
 
   state = {
-    res_prices: [
-      {
-        label: '便宜',
-        checked: false
-      },
-      {
-        label: '還可以',
-        checked: false
-      },
-      {
-        label: '貴',
-        checked: false
-      }
-    ],
-    // 中式、火鍋、西式、日式、異國料理、便當、麵店、速食、素食、小吃、早餐店、垃圾食物
-    res_types: [
-      {
-        label: '中式',
-        checked: false
-      },
-      {
-        label: '西式',
-        checked: false
-      },
-      {
-        label: '日式',
-        checked: false
-      },
-      {
-        label: '異國料理',
-        checked: false
-      },
-      {
-        label: '便當',
-        checked: false
-      },
-      {
-        label: '麵店',
-        checked: false
-      },
-      {
-        label: '火鍋',
-        checked: false
-      },
-      {
-        label: '速食',
-        checked: false
-      },
-      {
-        label: '素食',
-        checked: false
-      },
-      {
-        label: '早餐',
-        checked: false
-      },
-      {
-        label: '垃圾食物',
-        checked: false
-      },
-
-    ]
+    res_prices: []
   };
 
   onSelectorChange = (targetIndex) => {
@@ -82,6 +22,7 @@ class choosePanelBuilder extends Component {
   };
 
   render() {
+    console.log('restChoices', restChoices);
     return (
         <div className="main-selector-panel">
           <ChoosePanel
