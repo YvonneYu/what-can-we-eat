06ca2d243ded31ec6aeb27cc90e3e7095b1e55cf
Yvonne Yu <monkey6277@gmail.com>
2019-07-16 10:49:03 +0800

    update error message position

--- src/components/ChoosePanel/RestaurantInput.js
+++ src/components/ChoosePanel/RestaurantInput.js
@@ -5,11 +5,10 @@ const restaurantInput = (props) => {
   let labelName = 'res_' + props.id;
   return (
     <div className="cell">
-      <label htmlFor={ labelName }>{ props.label }</label>
+      <label htmlFor={ labelName }>{ props.label } { props.isValid ? null : <span className="form-error"> { props.errorMessage } </span> } </label>
       <input type={ props.type } name={ labelName } id={ labelName } value={ props.value }
              onChange={ (event) => props.onChange(event, props.id) }
              placeholder={props.placeholder} />
-      { props.isValid ? null : <p className="form-error"> { props.errorMessage } </p> }
     </div>
    )
 };
