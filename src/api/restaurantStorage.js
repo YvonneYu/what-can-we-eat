d02a7e11c1a238ad79a3769a8f26063064844985
Yvonne Yu <monkey6277@gmail.com>
2019-07-16 18:02:22 +0800

    develop local storage

---
+++ src/api/restaurantStorage.js
@@ -0,0 +1,19 @@
+/**
+ * Get and Save restaurant list using localstorage
+ */
+
+// 模擬 call api
+const TIMEOUT = 1000;
+const STORE_ID = 'restaurants_list';
+
+export default {
+  geRestList: new Promise(function(resolve) {
+      setTimeout(() => {
+        let list = localStorage.getItem(STORE_ID);
+        resolve(list && JSON.parse(list));
+      }, TIMEOUT);
+    }),
+  saveRestList: (value) => {
+    localStorage.setItem(STORE_ID, JSON.stringify(value));
+  }
+}
