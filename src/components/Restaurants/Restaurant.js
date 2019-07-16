07644c36b19460e48a3e49154e14f0adde33b172
Yvonne Yu <monkey6277@gmail.com>
2019-07-16 22:40:59 +0800

    add edit and delete button for rest card

--- src/components/Restaurants/Restaurant.js
+++ src/components/Restaurants/Restaurant.js
@@ -6,11 +6,17 @@ const restaurant = (props) => {
   return (
     <div className="cell">
       <div className="card card-size">
-          <div className="card-section">
-            <p>名稱：{props.name} </p>
-            <p>電話：{ props.tel? (<a href={ 'tel:' + props.tel }>{props.tel}</a>) : null}</p>
-            <p>地址：{props.address} </p>
+        <div className="card-section">
+          <p>名稱：{props.name} </p>
+          <p>電話：{props.tel ? (<a href={'tel:' + props.tel}>{props.tel}</a>) : null}</p>
+          <p>地址：{props.address} </p>
+        </div>
+        <div className="grid-x align-right">
+          <div className="small button-group cell small-6">
+            <a className="button alert">刪除</a>
+            <a className="button">編輯</a>
           </div>
+        </div>
       </div>
     </div>
   )
