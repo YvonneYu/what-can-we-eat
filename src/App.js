f4c230dd148cd7d5b0a460be16ad4c66db8f079a
Yvonne Yu <monkey6277@gmail.com>
2019-07-17 23:24:02 +0800

    add BroswerRouter dom

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Layout from './components/Layout/Layout.js';
 import RestaurantsBuilder from './containers/RestaurantsBuilder/RestaurantsBuilder';
 import ChoicesPanelBuilder from './containers/ChoicesPanelBuilder/ChoicesPanelBuilder';
-import {connect} from "react-redux";
+import {connect} from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 
 function App(props) {
   return (
-    <div>
-      <Layout>
-        <RestaurantsBuilder></RestaurantsBuilder>
-        <ChoicesPanelBuilder></ChoicesPanelBuilder>
-      </Layout>
+    <BrowserRouter>
+      <div>
+        <Layout>
+          <RestaurantsBuilder></RestaurantsBuilder>
+          <ChoicesPanelBuilder></ChoicesPanelBuilder>
+        </Layout>
+      </div>
       <div className={ 'is-loading ' + (props.isLoading && 'is-visible') }>
         <div className="sp sp-circle"></div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
