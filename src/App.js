f9821d8c386b4cbb964e0efdf3d93dfb4049186d
Yvonne Yu <monkey6277@gmail.com>
2019-07-17 18:19:29 +0800

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import Layout from './components/Layout/Layout.js';
 import RestaurantsBuilder from './containers/RestaurantsBuilder/RestaurantsBuilder';
 import ChoicesPanelBuilder from './containers/ChoicesPanelBuilder/ChoicesPanelBuilder';
+import {connect} from "react-redux";
 import './App.scss';
 
-function App() {
+function App(props) {
   return (
     <div>
       <Layout>
         <RestaurantsBuilder></RestaurantsBuilder>
         <ChoicesPanelBuilder></ChoicesPanelBuilder>
       </Layout>
+      <div className={ props.isLoading && 'is-loading'}>
+        <div className="sp sp-circle"></div>
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+    isLoading: state.restaurantList.isLoading
+  }
+);
+
+export default connect(mapStateToProps)(App);
