915c541f72fd1de153751315d99d1e3eccac544a
Yvonne Yu <monkey6277@gmail.com>
2019-07-19 11:42:20 +0800

    add PUBLIC_URL for fixing gh-pages router not work well problem

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.scss';
 
 function App(props) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
         <Layout>
           <Route path="/" exact component={ FindPanelBuilder }></Route>
